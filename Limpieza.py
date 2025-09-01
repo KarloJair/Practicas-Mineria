2aa57b27cf7cd052233ee2601f96f207de956f8c
Karlo Jair <karlo-sonic1@hotmail.com>
2025-09-01 11:19:12 -0600

    Data Cleaning - Practica 1

--- Limpieza.py
+++ Limpieza.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("vgsales.csv", encoding="latin-1")
+df = pd.read_csv("base vgsales.csv", encoding="latin-1")
 
 print(df.head())
 print(df.shape)
 print(df.info())
 print(df.describe())
-
 
 print("Valores nulos por columna:\n", df.isnull().sum())
 df = df.dropna()
@@ -14,12 +13,7 @@ df = df.dropna()
 # Eliminamos datos duplicados si es que los hay
 df = df.drop_duplicates()
 
-
-
 # Guardamos el dataset
-df.to_csv("vgsales cleaned.csv", index=False)
+df.to_csv("vgsales.csv", index=False)
 
-
-
-
-print("Dataset limpio guardado como dataset_limpio.csv")
+print("Dataset limpio guardado como vgsales.csv")
